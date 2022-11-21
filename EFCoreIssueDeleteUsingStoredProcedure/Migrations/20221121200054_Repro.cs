0b9db4bf484afb1c42a6fb55a2d4e0b0a92bea06
Marc-André Boivin <maboivin@experlogix.com>
2022-11-21 15:20:27 -0500

    Add project files.

---
+++ EFCoreIssueDeleteUsingStoredProcedure/Migrations/20221121200054_Repro.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreIssueDeleteUsingStoredProcedure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Repro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parent",
+                columns: table => new
+                {
+                    ParentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWSEQUENTIALID()"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parent", x => x.ParentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Child",
+                columns: table => new
+                {
+                    ChildId = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWSEQUENTIALID()"),
+                    ParentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Child", x => x.ChildId);
+                    table.ForeignKey(
+                        name: "FK_Child_Parent_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Parent",
+                        principalColumn: "ParentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Child_ParentId",
+                table: "Child",
+                column: "ParentId");
+
+            migrationBuilder.Sql(@"
+create procedure [dbo].[DeleteParent] (@parentId uniqueidentifier)
+as
+begin
+    set nocount on;
+
+    declare @result int;
+
+    begin transaction;
+
+    delete [p]
+    from [dbo].[Parent] [p]
+    where [p].[ParentId] = @parentId;
+
+    set @result = @@rowcount;
+
+    if (@result = 1)
+    begin
+        commit transaction;
+    end
+    else
+    begin
+        rollback transaction;
+    end
+
+    return @result;
+end;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Child");
+
+            migrationBuilder.DropTable(
+                name: "Parent");
+        }
+    }
+}
---
+++ EFCoreIssueDeleteUsingStoredProcedure/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using (var context = new ReproDbContext())
+{
+    context.Database.Migrate();
+
+    var parent = new Parent() { Name = "parent" };
+    context.Add(parent);
+    context.SaveChanges();
+
+    var child = new Child() { ParentId = parent.ParentId, Name = "child" };
+    context.Add(child);
+    context.SaveChanges();
+
+    context.Remove(parent);
+    context.SaveChanges();
+}
+
+class Parent
+{
+    public Guid ParentId { get; set; }
+    public string Name { get; set; }
+}
+
+class Child
+{
+    public Guid ChildId { get; set; }
+    public Guid ParentId { get; set; }
+    public Parent Parent { get; set; }
+    public string Name { get; set; }
+}
+
+class ReproDbContext : DbContext
+{
+    public DbSet<Parent> Parents { get; set; }
+    public DbSet<Child> Children { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var parentBuilder = modelBuilder.Entity<Parent>();
+        parentBuilder.ToTable(nameof(Parent));
+        parentBuilder.HasKey(p => p.ParentId);
+        parentBuilder.Property(p => p.ParentId).HasDefaultValueSql("NEWSEQUENTIALID()");
+        parentBuilder.Property(p => p.Name).IsRequired();
+
+        parentBuilder.DeleteUsingStoredProcedure(
+            "DeleteParent",
+            sp =>
+            {
+                sp.HasOriginalValueParameter(p => p.ParentId);
+                sp.HasRowsAffectedReturnValue();
+            });
+
+        var childBuilder = modelBuilder.Entity<Child>();
+        childBuilder.ToTable(nameof(Child));
+        childBuilder.HasKey(c => c.ChildId);
+        childBuilder.Property(c => c.ChildId).HasDefaultValueSql("NEWSEQUENTIALID()");
+        childBuilder.Property(c => c.Name).IsRequired();
+        childBuilder.HasOne(c => c.Parent).WithMany().HasForeignKey(c => c.ParentId);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=tcp:localhost,1433;Initial Catalog=EFCoreIssueDeleteUsingStoredProcedure;Persist Security Info=True;Integrated Security=true;Encrypt=True;TrustServerCertificate=True");
+    }
+}
